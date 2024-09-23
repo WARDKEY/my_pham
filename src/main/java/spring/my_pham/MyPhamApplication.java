8f9c5a6e6c024abe15eb92c64892a13afeabec74
WARDKEY <tjrmsduq@naver.com>
2024-09-23 18:45:55 +0900

    프로젝트 생성

---
+++ src/main/java/spring/my_pham/MyPhamApplication.java
@@ -0,0 +1,13 @@
+package spring.my_pham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPhamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/my_pham/MyPhamApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.my_pham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
